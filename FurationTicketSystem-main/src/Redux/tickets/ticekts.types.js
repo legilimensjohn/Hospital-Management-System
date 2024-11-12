793d55ef963432db65d2428bff6ba707294e296f
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-11-12 07:12:32 -0800

    Delete FurationTicketSystem-main directory

--- FurationTicketSystem-main/src/Redux/tickets/ticekts.types.js
+++
@@ -1,5 +0,0 @@
-export const GET_TICKETS_LOADING = "get/ticket/loading";
-export const GET_TICKETS_SUCCESS = "get/ticket/success";
-export const GET_TICKETS_ERROR = "get/ticket/error";
- export const GET_BUSID_SUCCESS = "get/busid/success";
- 
